83975ad854e1d5e159a768b2b4ea2f74eeb09568
oussamaM1 <makhloukoussama1@gmail.com>
2021-09-16 16:55:57 +0100

    do command

--- cmd/do.go
+++ cmd/do.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -10,7 +11,17 @@ var doCmd = &cobra.Command{
 	Use:   "do",
 	Short: "Mark a task as complete",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("do command called")
+		var ids []int
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				fmt.Println("Cannot convert the argument : ", arg)
+			} else {
+				ids = append(ids, id)
+			}
+		}
+		fmt.Println(ids)
+
 	},
 }
 
