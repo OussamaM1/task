7a6578ca21417c54d93a488e3a2e3d26353c84d0
Oussama Makhlouk <makhloukoussama1@gmail.com>
2024-03-27 16:33:32 +0000

    🎨 Beatify output when listing all tasks

--- go.mod
+++ go.mod
@@ -10,5 +10,9 @@ require (
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jedib0t/go-pretty/v6 v6.5.6 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 )
---
+++ services/output.go
@@ -0,0 +1,34 @@
+// Package services provides functionality related to configuration and managing todos file.
+package services
+
+import (
+	"fmt"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/oussamaM1/task/models"
+	"os"
+)
+
+// PrintTasks func - Prints the current tasks in form of table
+func PrintTasks(taskList []models.Task) {
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"#", "Description", "State"})
+	t.AppendSeparator()
+	for _, task := range taskList {
+		t.AppendRow([]interface{}{task.ID, task.Description, stateColor(task.State)})
+		t.AppendSeparator()
+	}
+	t.SetStyle(table.StyleLight)
+	t.Render()
+}
+
+// stateColor func - Give a custom color output to the state of task
+func stateColor(state string) string {
+	switch state {
+	case "In-progress":
+		return fmt.Sprintf("%s%s%s\n", colorBlue, state, colorReset)
+	case "Completed":
+		return fmt.Sprintf("%s%s%s\n", colorGreen, state, colorReset)
+	}
+	return state
+}
